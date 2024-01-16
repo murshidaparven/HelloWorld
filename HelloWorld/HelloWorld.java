81bb95ad0c3af5de15ed7e870ba648ab7579a2f7
murshida <murshida.parven@pcc.edu>
2024-01-15 23:56:25 -0800

    Updated author name and version

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,13 +2,13 @@
 /**
  * Write a description of class HelloWorld here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Murshida Parven
+ * @version Ver 1
  */
 public class HelloWorld
 {
    public static void main(String []args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
     }
 }
