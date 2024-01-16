3699fe06ff438ae06b1a1bc375ae97a9e9c170be
murshida <murshida.parven@pcc.edu>
2024-01-16 00:16:11 -0800

    say hello by name

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,7 @@
 
 /**
  * Write a description of class HelloWorld here.
+ * Say hello by name.
  *
  * @author Murshida Parven
  * @version Ver 1
@@ -9,6 +10,15 @@ public class HelloWorld
 {
    public static void main(String []args)
     {
-        System.out.println("Hello World1");
+        System.out.println("Hello World!");
+        
+    }
+    
+    /**
+     * Say hello by name
+     */
+    public void sayHello(String name)
+    {
+        System.out.println("Hello " + name + "!");
     }
 }
